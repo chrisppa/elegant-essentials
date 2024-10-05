17d5a6e2f5e413a39207027c3cb0be24888dfbf9
crisppycodes <cturyasiima@gmail.com>
2024-10-05 21:49:42 +0300

    fix price formating and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = "";
 cart.forEach((cartItem) => {
@@ -27,9 +28,11 @@ cart.forEach((cartItem) => {
           <div class="product-name">
             ${matchingItem.name}
           </div>
-          <div class="product-price">$10.90</div>
+          <div class="product-price">$${formatCurrency(matchingItem.priceCents)}</div>
           <div class="product-quantity">
-            <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
+            <span> Quantity: <span class="quantity-label">${
+              cartItem.quantity
+            }</span> </span>
             <span class="update-quantity-link link-primary">
               Update
             </span>
@@ -48,7 +51,7 @@ cart.forEach((cartItem) => {
               type="radio"
               checked
               class="delivery-option-input"
-              name="delivery-option-1"
+              name="delivery-option-${matchingItem.id}"
             />
             <div>
               <div class="delivery-option-date">Tuesday, June 21</div>
@@ -59,7 +62,7 @@ cart.forEach((cartItem) => {
             <input
               type="radio"
               class="delivery-option-input"
-              name="delivery-option-1"
+              name="delivery-option-${matchingItem.id}"
             />
             <div>
               <div class="delivery-option-date">Wednesday, June 15</div>
@@ -70,7 +73,7 @@ cart.forEach((cartItem) => {
             <input
               type="radio"
               class="delivery-option-input"
-              name="delivery-option-1"
+              name="delivery-option-${matchingItem.id}"
             />
             <div>
               <div class="delivery-option-date">Monday, June 13</div>
