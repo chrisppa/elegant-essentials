ba302b1dc3266b1e96438b13031e292a5c89d9bd
crisppycodes <cturyasiima@gmail.com>
2024-10-17 12:47:51 +0300

    remove default tests and add money tests

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,13 +1,15 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
-console.log('converts cents to dollars')
+console.log("test suite: formatCurrency");
+
+console.log("converts cents to dollars");
 if (formatCurrency(2095) === "20.95") {
   console.log("passed");
 } else {
   console.log("failed");
 }
 
-console.log('Works with zero');
+console.log("Works with zero");
 
 if (formatCurrency(0) === "0.00") {
   console.log("passed");
@@ -15,7 +17,7 @@ if (formatCurrency(0) === "0.00") {
   console.log("failed");
 }
 
-console.log('Rounds up to the nearest cent');
+console.log("Rounds up to the nearest cent");
 
 if (formatCurrency(2000.5) === "20.01") {
   console.log("passed");
