240a0e6c7cc63e28248758b02be17065aba2ded2
crisppycodes <cturyasiima@gmail.com>
2024-10-25 22:04:09 +0300

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,8 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/order-summary.js";
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
 // import "../data/cart-class.js";
+import "../data/backend-practice.js";
 
 renderCheckoutHeader();
-renderOrderSummary();  
+renderOrderSummary();
 renderPaymentSummary();
